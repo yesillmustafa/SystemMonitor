c8c8896d77279af265e7b1d6f24159041e8fd513
Mustafa Yeşil <dev.mustafayesil@gmail.com>
2026-01-30 15:04:44 +0300

    Add exception handling to application entry point

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,21 @@
 
 int main()
 {
-    Application app;
-    app.Run();
+    try
+    {
+        Application app;
+        app.Run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Unhandled exception: " << e.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unhandled unknown exception" << std::endl;
+        return 1;
+    }
     return 0;
 }
 
